1df62bdddf4185165b8f311c0efe0bd3aee32ee7
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-06-13 17:07:57 +0530

    completed

--- src/components/hero-section/hero.jsx
+++ src/components/hero-section/hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './Hero.css'
 import {HiLocationMarker} from 'react-icons/hi'
 import CountUp from "react-countup";
+import {motion} from 'framer-motion'
 
 const Hero = () => {
   return (
@@ -10,8 +11,8 @@ const Hero = () => {
         <div className="flexColStart hero-left">
 
           <div className="hero-title">
-            <div className="orange-circle"/>
-            <h1>Discover <br /> Most Suitable <br />Property</h1>
+            <div className="orange-circle"/>      
+            <h1>Discover <br /> Most Suitable <br />Property</h1>            
           </div>
           <div className="flexColStart hero-description">
             <span className="secondaryText" >Find a variety of properties that suit you very easilty</span>
